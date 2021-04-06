9773d97346164194f2cc492fab066c9af11962a5
JIALIN-HUANG <78190123+JIALIN-HUANG@users.noreply.github.com>
2021-04-05 17:50:56 -0700

    upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Hello Chairs");
 
 var Airtable = require("airtable");
-console.log(Airtable);
+//console.log(Airtable);
 
 // use the airtable librar to get a variable that represents one of our bases
 var base = new Airtable({ apiKey: "keyj8qNsToxQPYuPk" }).base(
@@ -37,7 +37,11 @@ function gotAllChairs(err) {
 
   // call functions to log and show the books
   consoleLogChairs();
-  showChairs();
+  try {
+   showChairs();
+  } catch(e){
+   console.log(e);
+  }
 }
 
 // just loop through the books and console.log them
@@ -52,9 +56,93 @@ function consoleLogChairs() {
 function showChairs() {
   console.log("showChairs()");
   chairs.forEach((chair) => {
-    const h2 = document.createElement("h2");
-    h2.innerText = chair.fields.title;
-    document.body.appendChild(h2);
+
+    //const h2 = document.createElement("h2");
+   // h2.innerText = chair.fields.name;
+   // document.body.appendChild(h2);
+
+   //create new div container
+   var chairContainer = document.createElement("div");
+   chairContainer.classList.add("chair-container");
+   document.querySelector(".container").append(chairContainer);
+
+  //add chair name to our container
+  var chairName = document.createElement("h2");
+  chairName.classList.add("chair-Name");
+  chairName.innerText = chair.fields.name;
+  chairContainer.append(chairName);
+
+   //add designer
+   var chairDesigner = document.createElement("h2");
+   chairDesigner.classList.add("designer");
+   chairDesigner.innerText = chair.fields.designer;
+   chairContainer.append(chairDesigner);
+
+    //add chair image
+    var chairImg = document.createElement("img");
+    chairImg.classList.add("chair-img");
+    chairImg.src = chair.fields.chairImage[0].url;
+    chairContainer.append(chairImg);
+
+      //get genre field from airtable
+
+var chairGenre = chair.fields.genre;
+chairGenre.forEach(function(genre) {
+  chairContainer.classList.add(genre)
+})
+
+//---------------------------add filter!!!!!!--------------------
+//add event lisnter to our filter
+//to add an active class to our song
+
+var filterWood = document.querySelector('.wood');
+filterWood.addEventListener("click", function(){
+
+  if(chairContainer.classList.contains("wood")){
+    chairContainer.style.background = "pink";
+  } else{
+    chairContainer.style.background = "white";
+  }
+})
+
+//active
+chairContainer.addEventListener("click", function(){
+  chairImg.classList.toggle("active");
+})
+
+//----------------1950s---------------------------
+var filter1950 = document.querySelector('.1950s');
+filter1950.addEventListener("click", function(){
+
+  if(chairContainer.classList.contains("1950s")){
+    chairContainer.style.background = "yellow";
+  } else{
+    chairContainer.style.background = "white";
+  }
+})
+
+//----------------20s---------------------------
+var filter20 = document.querySelector('.20s');
+filter20.addEventListener("click", function(){
+
+  if(chairContainer.classList.contains("20s")){
+    chairContainer.style.background = "yellow";
+  } else{
+    chairContainer.style.background = "white";
+  }
+})
+
+//----------------marco---------------------------
+var filterMarco = document.querySelector('.marco');
+filterMarco.addEventListener("click", function(){
+
+  if(chairContainer.classList.contains("marco")){
+    chairContainer.style.background = "yellow";
+  } else{
+    chairContainer.style.background = "white";
+  }
+})
+
   });
 }
 
